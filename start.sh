5b9649b3b145aa96cdc929757bb5340dbdc53d84
Sai <shemchandra@charterschoolsusa.com>
2018-06-28 18:41:48 -0400

    wip

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/env sh
-#virtualenv env -ppython3
-#. env/bin/activate
-#pip install -r requirements.txt -t modules
-export PYTHONPATH=modules
-python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8080
